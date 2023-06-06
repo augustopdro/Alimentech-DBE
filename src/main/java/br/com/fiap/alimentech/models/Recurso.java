5c938659d8ed406137c997a7cc2b05223d3e07ed
rm94990@fiap.com.br <rm94990@fiap.com.br>
2023-06-06 18:21:49 -0300

    added models

---
+++ src/main/java/br/com/fiap/alimentech/models/Recurso.java
@@ -0,0 +1,40 @@
+package br.com.fiap.alimentech.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "T_RECURSO")
+public class Recurso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "area_terreno")
+    private String areaDoTerreno;
+
+    @NotNull
+    @Column(name = "reserva_dinheiro")
+    private String dinheiroDisponivel;
+
+    @NotNull
+    @Column(name = "prazo_producao")
+    private String prazoParaColheita;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    @JsonIgnore
+    private Usuario usuario;
+
+    public Recurso(String areaDoTerreno, String dinheiroDisponivel, String prazoParaColheita) {
+        this.areaDoTerreno = areaDoTerreno;
+        this.dinheiroDisponivel = dinheiroDisponivel;
+        this.prazoParaColheita = prazoParaColheita;
+    }
+}
