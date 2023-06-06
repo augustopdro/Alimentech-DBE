a0294cf902cd08d6f104520a68e5d2a960033a75
rm94990@fiap.com.br <rm94990@fiap.com.br>
2023-06-06 18:22:06 -0300

    added repositories

---
+++ src/main/java/br/com/fiap/alimentech/repositories/RecursoRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.alimentech.repositories;
+
+import br.com.fiap.alimentech.models.Recurso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecursoRepository extends JpaRepository<Recurso, Long> {
+
+}
